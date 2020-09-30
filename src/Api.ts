885e47e99afbfc2c5bc4b65b8a7ec9bc5f3010db
TKTran <tranthienkhoi@gmail.com>
2020-09-30 21:47:12 +0700

    Updated export API

--- src/Api.ts
+++ src/Api.ts
@@ -1,11 +1,15 @@
 import VehicleApi from './Api/VehicleApi';
 import FavoriteApi from './Api/FavoriteApi';
 import UserApi from './Api/UserApi';
+import EmployeeApi from './Api/EmployeeApi';
+import ManageApi from './Api/ManageApi';
 import Environment from './Enum/Environment';
 
 export {
   VehicleApi as Vehicle,
   FavoriteApi as Favorite,
   UserApi as User,
+  EmployeeApi as EmployeeApi,
+  ManageApi as Manage,
   Environment
 };
