7e25371f5d20b72dd4537a7afc8584b98ed625f8
TKTran <tranthienkhoi@gmail.com>
2020-10-04 16:53:48 +0700

    Added vehicle category

---
+++ src/Model/IVehicleCategory.ts
@@ -0,0 +1,5 @@
+import IVehicleCategoryResponse from '../ResponseModel/IVehicleCategoryResponse';
+
+export default interface IVehicleCategory extends IVehicleCategoryResponse {
+  isMotorbike: boolean;
+}
