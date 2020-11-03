615bb388260c92d013f7ff20fee3ace33a8ab848
KayoTTran <ktran@inquirer.com>
2020-11-03 14:27:10 +0700

    Updated invoice response

--- src/Model/IInvoice.ts
+++ src/Model/IInvoice.ts
@@ -6,7 +6,12 @@ export default interface IInvoice {
   id?: number;
   garage_id: number;
   appointment_id: number;
+  title: string;
+  image: string;
+  date_invoice: string;
   user_id: number;
+  m_service_garages: string;
+  service_name: string[];
   discount: string;
   comments: string;
   content_refuse: string;
